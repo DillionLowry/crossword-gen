3522e237a49d6ce6213d8e2930d2b48db8a9d799
Dillion Lowry <dll3k@mtmail.mtsu.edu>
2021-11-09 22:24:01 -0600

    Able to generate a board without constraints. Need to add regex

--- crossword-gen.py
+++ crossword-gen.py
@@ -1,16 +1,35 @@
+import random
+import re
+
 class Coord(object):
-    def __init__(self, start, end, vert):
+    def __init__(self, start, end, vert, num):
         self.start = start
         self.end = end
         self.length = (end[0] - start[0]) + (end[1] - start[1]) + 1
         self.vertical = vert
+        self.number = num
+        self.collisions =[]
 
-        def __str__(self):
-            return self.start, self.end, self.length
+    def __str__(self):
+        return str(self.start)+ str(self.end)+ " "+str(self.number)+" "+self.get_direction()
+    
+    def get_direction(self):
+        if self.vertical:
+            return "down"
+        else:
+            return "across"
+
+
+class Word(object):
+    def __init__(self, coord, word_tuple):
+        self.coord = coord
+        self.text = word_tuple[0]
+        self.clue = word_tuple[1]
 
 class Board(object):
     def __init__(self, shape):
         self.shape = shape
+        self.generated = shape
         self.coords = []
         self.width = len(shape[0])
         self.height = len(shape)
@@ -23,7 +42,146 @@ class Board(object):
 
     def print_coords(self):
         for coord in self.coords:
-            print(coord.start, coord.end, coord.length)
+            print(coord.start, coord.end, coord.length, coord.number, coord.get_direction())
+            for ccoord in coord.collisions:
+                print(ccoord)
+    
+    def print_board(self):
+        for row in self.generated:
+            for char in row:
+                print(char, end='')
+            print()
+
+def generate_coordinates(crossword):
+    num_rows = crossword.height
+    num_cols = crossword.width
+    '''
+    for r in range(num_rows):
+        for c in range(num_cols):
+            print(crossword.shape[r][c], end=" ")
+        print()
+    '''
+    horizontal_num = 1
+    vertical_num = 1
+
+
+    # find coords by row
+    for r in range(num_rows):
+        start = None
+        end = None
+        for c in range(num_cols):
+            # start a new word
+            if crossword.shape[r][c]=='-' and not start:
+                # it isn't just one character
+                if c+1 < num_cols and crossword.shape[r][c+1] != 'X':
+                    start = (r,c)
+                    crossword.shape[r][c]=horizontal_num
+                else:
+                    continue
+            # continue word
+            if start and not end:
+                crossword.shape[r][c]=horizontal_num
+                # This is the last letter
+                if (c+1==num_cols or crossword.shape[r][c+1]=='X'):
+                        end = (r,c)
+                        h_coord = Coord(start, end, False, horizontal_num)
+                        crossword.add_coord(h_coord)
+                        horizontal_num+=1
+                        start = None
+                        end = None
+                        continue
+                #print(r, "  ", c)
+        
+    #print("---------------")
+
+    # find coords by col
+    for c in range(num_cols):
+        start = None
+        end = None
+        for r in range(num_rows):
+             # start a new word, either the across number or '-'
+            if (isinstance(crossword.shape[r][c], int) or crossword.shape[r][c]=='-') and not start:
+                # it isn't just one character
+                if r+1 < num_rows and crossword.shape[r+1][c] != 'X':
+                    start = (r,c)
+                else:
+                    continue
+            # continue word
+            if start and not end:
+                # This is the last letter
+                if (r+1==num_rows or crossword.shape[r+1][c]=='X'):
+                        end = (r,c)
+                        v_coord = Coord(start, end, True, vertical_num)
+
+                        # collisions
+                        for x in range(start[0],end[0]+1):
+                            if (isinstance(crossword.shape[x][c], int)):
+                                crossword.coords[crossword.shape[x][c]-1].collisions.append(v_coord)
+                                v_coord.collisions.append(crossword.coords[crossword.shape[x][c]-1])
+                        crossword.add_coord(v_coord)
+                        vertical_num+=1
+                        start = None
+                        end = None
+                        continue
+            #print(r, "  ", c)
+
+def generate_collisions(board):
+    for coord in board.coords:
+        pass
+    
+def generate_crossword(board, wordlist):
+    
+    tried_words = set()
+    collision_list = []
+
+    #place first word
+    #place_word(board, board.coords[0], wordlist, None)
+    '''
+    for coord in board.coords[0].collisions:
+        collision_list.append(coord)
+    
+    for coord in collision_list:
+        
+        for coll in coord.collisions:
+            if coll not in collision_list:
+                print('Added: ', coord)
+                collision_list.append(coll)
+    
+    '''
+    find_spot(board, board.coords[0], collision_list, wordlist)
+
+    print("hello what the fuck")
+    
+    for coord in collision_list:
+        print("c- ", coord)
+    
+def find_spot(board, coord, collision_list, wordlist):
+    #all_placed = True
+    print("FINDING A SPOT")
+    print(coord)
+    if coord == None:
+        return
+    for col in coord.collisions:
+        if col and col not in collision_list:
+            #all_placed = False
+            collision_list.append(col)
+            find_spot(board, col, collision_list, wordlist)
+
+    constraints = "test"
+    place_word(board,coord,wordlist, None)
+   
+def place_word(board, coord, words, constraints):
+    if constraints==None:
+        print(coord.length)
+        word = words[coord.length-2][random.randrange(len(words[coord.length-2]))]
+        print(word)
+        for x in range(coord.length):
+            if coord.vertical:
+                board.generated[coord.start[0]+x][coord.start[1]] = word[0][x]
+            else:
+                board.generated[coord.start[0]][coord.start[1]+x] = word[0][x]
+
+
 
 def main():
     #filename = input("Input file name: ")
@@ -31,7 +189,7 @@ def main():
     f = open(filename, "r")
 
     # Wordlist is a 2d list of tuples
-    # 0 index is two letter words, max length of 16 letters
+    # 0 index is two letter words, max length of 16 letters at index 14
     wordlist = [[] for x in range(15)]
 
     while True:
@@ -68,7 +226,7 @@ def main():
     test3 = [["-","-","-","X","X","-","-","-"],
             ["-","-","-","X","-","-","-","-"],
             ["-","-","-","-","-","-","-","-"],
-            ["-","X","X","-","X","X","X","X"],
+            ["-","X","X","-","X","-","X","X"],
             ["-","-","-","X","X","-","-","-"],
             ["-","-","-","-","X","-","-","-"],
             ["-","-","-","-","X","-","-","-"]]
@@ -82,67 +240,15 @@ def main():
             ["-","-","-","-","X"]]
 
     crossword = Board(test3)
-
-    num_rows = crossword.height
-    num_cols = crossword.width
-
-    for r in range(num_rows):
-        for c in range(num_cols):
-            print(crossword.shape[r][c], end=" ")
-        print()
-
-    # find coords
-
-    #l = end - start + 1
-    
-    for r in range(num_rows):
-        start = None
-        end = None
-        for c in range(num_cols):
-            if crossword.shape[r][c]=='-' and not start:
-                start = (r,c)
-            if (crossword.shape[r][c]=='X' or c == num_cols-1) and start and not end:
-                if crossword.shape[r][c]=='X':
-                    end = (r,c-1)
-                else:
-                    end = (r,c)
-                h_coord = Coord(start, end, False)
-                crossword.add_coord(h_coord)
-                start = None
-                end = None
-            print(r, "  ", c)
-        
-    print("---------------")
-
-    for c in range(num_cols):
-        start = None
-        end = None
-        for r in range(num_rows):
-            if crossword.shape[r][c]=='-' and not start:
-                start = (r,c)
-            if (crossword.shape[r][c]=='X' or r == num_rows-1) and start and not end:
-                if crossword.shape[r][c]=='X':
-                    end = (r-1,c)
-                else:
-                    end = (r,c)
-                v_coord = Coord(start, end, True)
-                crossword.add_coord(v_coord)
-                start = None
-                end = None
-            print(r, "  ", c)
-
-    #print(crossword.get_coords())
-
+    generate_coordinates(crossword)
     crossword.print_coords()
+    crossword.print_board()
+    generate_crossword(crossword, wordlist)    
+    crossword.print_board()
 
 
 
-    tried_words = set()
-    #try to place word
-    #check surroundings
-    #attempt to place crossing words
 
 
 if __name__ == "__main__":
     main()
-
