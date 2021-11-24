912442e691316e4fcd2e5ffaf895ce418aded365
Dillion Lowry <dillionleel@gmail.com>
2021-11-24 16:24:55 -0600

    updated JSON processing for use in crossword-generator

--- api_requests.py
+++ api_requests.py
@@ -1,12 +1,14 @@
 import json
+from json import encoder
 import requests
+import html
 
 url = "https://www.xwordinfo.com/JSON/Data.aspx"
 
 # For whatever reason the API will not return data on a get without the "Referer" header
 my_headers = {
-"Accept": "*/*",
-"Accept-Encoding": "gzip, deflate, br",
+"Accept": "application/json",
+"Accept-Encoding": "utf-8",
 "Connection": "keep-alive",
 "Referer": "https://www.xwordinfo.com/JSON/",
 "Host": "www.xwordinfo.com"
@@ -15,5 +17,9 @@ my_headers = {
 date = "9/11/2008"
 
 res = requests.get(url, headers=my_headers, params="date:"+date)
-data=res.json()
-print(data["answers"]["across"])
+res.encoding='utf-8'
+data=res.json(encoding='utf-8')
+
+for x in range(len(data["answers"]["across"])):
+    # the api returns escaped character and numbers before the answers so split on that and take second element
+    print(html.unescape(data["answers"]["across"][x]), '\n', html.unescape(data["clues"]["across"][x]).split(". ")[1], sep="")
